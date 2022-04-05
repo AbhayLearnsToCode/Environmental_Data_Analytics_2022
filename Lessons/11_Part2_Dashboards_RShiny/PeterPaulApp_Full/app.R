739deaaaada97054bc6e78707f2ba2da075da863
AbhayLearnsToCode <abhay.vrao@duke.edu>
2022-04-05 18:17:02 -0400

    Commit/push on April 5

--- Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/app.R
+++ Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/app.R
@@ -11,14 +11,14 @@ nutrient_data <- nutrient_data %>%
   select(lakename, sampledate:po4)
 
 #### Define UI ----
-ui <- fluidPage(theme = shinytheme("yeti"),
-  titlePanel("Nutrients in Peter Lake and Paul Lake, Lookie Here"),
+ui <- fluidPage(theme = shinytheme("superhero"),
+  titlePanel("Nutrients in Peter Lake and Paul Lake"),
   sidebarLayout(
     sidebarPanel(
       
       # Select nutrient to plot
       selectInput(inputId = "y", 
-                  label = "Protein",
+                  label = "Nutrients",
                   choices = c("tn_ug", "tp_ug", "nh34", "no23", "po4"), 
                   selected = "tp_ug"),
       
@@ -63,12 +63,11 @@ server <- function(input, output) {
         ggplot(filtered_nutrient_data(), 
                aes_string(x = "sampledate", y = input$y, 
                           fill = "depth_id", shape = "lakename")) +
-          geom_point(alpha = 0.8, size = 2) +
+          geom_point(alpha = 0.8, size = 3) +
           theme_classic(base_size = 14) +
           scale_shape_manual(values = c(21, 24)) +
           labs(x = "Date", y = expression(Concentration ~ (mu*g / L)), shape = "Lake", fill = "Depth ID") +
-          scale_fill_distiller(palette = "YlOrBr", guide = "colorbar", direction = 1)
-          #scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
+          scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
       })
        
     # Create a table that generates data for each point selected on the graph  
@@ -84,18 +83,37 @@ shinyApp(ui = ui, server = server)
 
 #### Questions for coding challenge ----
 #1. Play with changing the options on the sidebar. 
-    # Choose a shinytheme that you like. The default here is "yeti"
-    # How do you change the default settings? 
-    # How does each type of widget differ in its code and how it references the dataframe?
+# Choose a shinytheme that you like. The default here is "yeti" 
+## Abhay:Tried Cerulean, Superhero, Slate and a few others.
+
+# How do you change the default settings? 
+## Changing the theme next to the "shinytheme" code
+
+# How does each type of widget differ in its code and how it references the dataframe?
+## Two widgets have x and y reference inputs which output the overall results,
+## the other widgets are filters that help obtain nuanced insights.
+
 #2. How is the mainPanel component of the UI structured? 
-    # How does the output appear based on this code?
+# How does the output appear based on this code?
+## The mainPanel component leads with a plot output, and then a table output. 
+## Bit unsure of what the table output is. The plot output syntax selects the 
+## shape and form of the output.
+
 #3. Explore the reactive formatting within the server.
-    # Which variables need to have reactive formatting? 
-    # How does this relate to selecting rows vs. columns from the original data frame?
+# Which variables need to have reactive formatting? 
+## The nutrients, dates, depthIDs, and lake names.
+
+# How does this relate to selecting rows vs. columns from the original data frame?
+# These variables ar eanalogous to column headings in a dataset
+
 #4. Analyze the similarities and differences between ggplot code for a rendered vs. static plot.
-    # Why are the aesthetics for x, y, fill, and shape formatted the way they are?
-    # Note: the data frame has a "()" after it. This is necessary for reactive formatting.
-    # Adjust the aesthetics, playing with different shapes, colors, fills, sizes, transparencies, etc.
+# Why are the aesthetics for x, y, fill, and shape formatted the way they are?
+## y is based on the dropdown option the user selects for nutrients;the x input is
+## the date range. The depth ID and lake name are filters. 
+
+# Note: the data frame has a "()" after it. This is necessary for reactive formatting.
+# Adjust the aesthetics, playing with different shapes, colors, fills, sizes, transparencies, etc.
+
 #5. Analyze the code used for the renderTable function. 
     # Notice where each bit of code comes from in the UI and server. 
     # Note: renderTable doesn't work well with dates. "sampledate" appears as # of days since 1970.
